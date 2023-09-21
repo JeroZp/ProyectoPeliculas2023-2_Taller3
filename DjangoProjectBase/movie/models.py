8b6ee2994935a0999e96cf36a3154559120ab7af
Jeronimo Perez Baquero <perezjeronimo35@gmail.com>
2023-09-21 11:25:50 -0500

    Taller 3

--- DjangoProjectBase/movie/models.py
+++ DjangoProjectBase/movie/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+import numpy as np
 
+def get_default_array():
+  default_arr = np.random.rand(1536)  # Adjust this to your desired default array
+  return default_arr.tobytes()
 
 class Movie(models.Model):
   title = models.CharField(max_length=100)
   description = models.CharField(max_length=1000)
-  image = models.ImageField(upload_to='movie/images/')
+  image = models.ImageField(upload_to='movie/images/', default = 'movie/images/default.jpg')
   url = models.URLField(blank=True)
-  
+  emb = models.BinaryField(default=get_default_array())
+ 
   def __str__(self):
     return self.title
 
